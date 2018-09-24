24e7484ed6a879b203294befd4f358ec4fa8ef1e
BrandonAmyot <amyot.brandon@hotmail.com>
2018-09-24 14:44:23 -0400

    works but is slow

--- Demo.java
+++ Demo.java
@@ -1,28 +1,35 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class Demo {
 	
 	public static void main(String[] args) throws Exception {
 		
+//		URL url = new URL("http://httpbin.org/get?course=networking&assignment=1");
 //		String hostName = "http://httpbin.org/get?course=networking&assignment=1";
 //		int portNumber = 80;
-
+		Socket mySocket;
+		
 		try {
-		   Socket echoSocket = new Socket("www.httpbin.org", 80);
-		   System.out.println(echoSocket.isConnected());
+		   mySocket = new Socket("www.mock-server.com", 80);
+		   System.out.println(mySocket.isConnected());
 		   
 //		   PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
-		   BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
-		   BufferedReader stdIn =new BufferedReader(new InputStreamReader(System.in));
+		   BufferedReader input = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
+//		   BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 		   
 		   String line;
-		   while((line = in.readLine()) != null) {
+		   while((line = input.readLine()) != null) {
+			   System.out.println("Hello");
 			   System.out.println(line);
 		   }
-		   echoSocket.close();
-		   in.close();
+		   System.out.println("Hello");
+		   mySocket.close();
+		   System.out.println(mySocket.isConnected());
+		   input.close();
 		}
 		catch(Exception e) {
 			e.printStackTrace();
@@ -31,7 +38,7 @@ public class Demo {
 		// GET request
 //        URLConnection newConn = url.openConnection();
 //        BufferedReader in = new BufferedReader(new InputStreamReader(newConn.getInputStream()));
-		
+//		
 //		BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8")); 
 //		for (String line; (line = reader.readLine()) != null;) {
 //			System.out.println(line);
