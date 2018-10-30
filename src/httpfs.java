1152d42a5a65d2c857c6e18e9b1a04d162d6f316
Brandon Amyot <amyot.brandon@hotmail.com>
2018-10-29 22:41:38 -0400

    File reader

--- src/httpfs.java
+++ src/httpfs.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -20,19 +21,16 @@ public class httpfs {
 		String path = serverDetails.substring(serverDetails.lastIndexOf(" ")+1);
 		
 		try {
-		ServerSocket server = new ServerSocket(port);
-        System.out.println("Listening for connection on port " + server.getLocalPort() + "...");
-        System.out.println("Files in directory " + path);
-        
-        //Read the files in a directory (https://stackoverflow.com/questions/5694385/getting-the-filenames-of-all-files-in-a-folder)
-        File folder = new File(path);
-        File[] listOfFiles = folder.listFiles();
-        for (int i = 0; i < listOfFiles.length; i++) {
-            System.out.println("File " + listOfFiles[i].getName());
-        }
-        
-        
-        
+			ServerSocket server = new ServerSocket(port);
+	        System.out.println("Listening for connection on port " + server.getLocalPort() + "...");
+	        System.out.println("Files in directory " + path);
+	        
+	        //Read the files in a directory (https://stackoverflow.com/questions/5694385/getting-the-filenames-of-all-files-in-a-folder)
+	        File folder = new File(path);
+	        File[] listOfFiles = folder.listFiles();
+	        for (int i = 0; i < listOfFiles.length; i++) {
+	            System.out.println("File " + listOfFiles[i].getName());
+	        }
         
             while (true) {
                 Socket clientSocket = server.accept();
@@ -43,11 +41,34 @@ public class httpfs {
                 // Prepare response
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);                
                 String response = "HTTP/1.0 200 OK\r\n";
-                 
-                while (!line.isEmpty()) {
-                    System.out.println(line);
-                    response += line + "\n";
-                    line = reader.readLine();
+                
+                String tempFileName = StringUtils.substringAfter(line, " /");
+        		String fileName = StringUtils.substringBefore(tempFileName, " ");
+                if(fileName != null) {
+                	try {
+            			File inputFile = new File(fileName);
+            			if(!inputFile.exists()) {            				
+            				response = "HTTP/1.0 404: The file " + fileName + " not found.\r\n";
+//            				out.println(response);
+            			}
+            			
+            			BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
+            			while((line = fileReader.readLine()) != null) {
+            				response += line;				
+            			}
+            			fileReader.close();
+            		}
+            		catch(IOException e) {
+            			e.printStackTrace();
+            		}
+                }
+                else {
+                	response = "HTTP/1.0 404: The file " + fileName + " not found.\r\n";
+                	while (!line.isEmpty()) {
+                		System.out.println(line);
+                		response += line + "\n";
+                		line = reader.readLine();
+                	}
                 }
                 // send response
                 out.println(response);
